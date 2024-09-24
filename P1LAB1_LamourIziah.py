fe4eff6f91d15901bd8f278044f4386f5737c06f
Zayzilla <lamouri9026@student.faytechcc.edu>
2024-09-24 14:22:31 -0400

    Add files via upload
    First Upload

---
+++ P1LAB1_LamourIziah.py
@@ -0,0 +1,15 @@
+# Iziah Lamour
+# 9/24/2024
+# P1LAB1
+# Get input from the user and display output.
+
+# Get first name from user
+firstname = input("enter your first name: ")
+
+# Get last anme from user
+lastname = input("enter your last name: ")
+
+# display info to the user
+print("Hello,", firstname, lastname + "! Welcome to CTI-110")
+
+
